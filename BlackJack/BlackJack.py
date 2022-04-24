4c9baa10635d4f86888878cb78e4819edcfdb02b
CJ Bauzon <72855434+cbauzon@users.noreply.github.com>
2022-04-24 03:47:12 -0700

    Handled some input errors that may occur with init_players()

--- BlackJack/BlackJack.py
+++ BlackJack/BlackJack.py
@@ -2,8 +2,7 @@ from BlackJack_classes import *
 from BlackJack_funcs import *
 
 def main():
-    num_players = get_num_players()
-    init_players(num_players)    
+    init_players()    
 
 if __name__ == "__main__":
     main()
--- BlackJack/BlackJack_funcs.py
+++ BlackJack/BlackJack_funcs.py
@@ -14,13 +14,32 @@ def get_num_players():
             
     return num_players
 
-def init_players(num_players):
-    player_list = []
-    money = input("How much money should each player start with?: ")
-    for i in range (0, num_players):
-        player = Player(input("Player " + str(i+1) + "'s name: "), money)
-        player_list.append(player)
+def init_players():
+    player_ready = False
+    while (player_ready == False):
+        num_players = get_num_players()
+
+        player_list = []
+        money = 0
+
+        while money <= 0:
+            try:
+                money = int(input("How much money should each player start with?: "))
+                if money <= 0:
+                    raise ValueError
+            except ValueError:
+                print("ERROR: The entry has to be a whole number!")
+
+        for i in range (0, num_players):
+            player = Player(input("Player " + str(i+1) + "'s name: "), money)
+            player_list.append(player)
     
-    print("The following players are playing: ")
-    for player in player_list:
-        print(player.name)
+        print("The following players are playing with ${money} each: ".format(money = money))
+        for player in player_list:
+            print(player.name)
+
+        ans = input("Is this correct? (y/n): ")
+        if ans == "y":
+            player_ready = True
+        else:
+            player_ready = False
