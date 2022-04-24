e3292abd01f943cda772943ef18a17082b390290
CJ Bauzon <72855434+cbauzon@users.noreply.github.com>
2022-04-23 22:18:58 -0700

    Add project files.

---
+++ BlackJack/BlackJack_classes.py
@@ -0,0 +1,12 @@
+class Player:
+
+    def _init_(self, name, money):
+        self.name = name
+        self.money = money
+
+    def check_money(self):
+        return ("You have " + "$" + self.money + " left.")
+
+    
+
+
